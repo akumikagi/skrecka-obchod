106f360f5ac33bdb76506be4886fb65e50078619
akumi <akumikagi@gmail.com>
2023-12-13 17:37:12 +0100

    Add splash page and footer

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const button = document.querySelector("#kupit");
-const emilka = document.querySelector("#img");
-const popis = document.querySelector("#popis");
+const buttonEmilka = document.querySelector("#emilka");
+const emilka = document.querySelector(".img");
+const popis = document.querySelector(".popis");
 
-button.addEventListener("click", bobikTatik);
+buttonEmilka.addEventListener("click", bobikTatik);
 
 function bobikTatik() {
   alert("Skrecka kupena!");
   emilka.setAttribute("src", "./images/skreckaKupena.png");
   popis.textContent = "Emilka: Claimnuta";
-  button.className = "disabled";
-  button.disabled = true;
+  buttonEmilka.className += " disabled";
+  buttonEmilka.disabled = true;
 }
