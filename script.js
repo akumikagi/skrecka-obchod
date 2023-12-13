cfcdc01c7c196554c775d38507b668a7c2620abe
akumi <akumikagi@gmail.com>
2023-12-13 16:00:50 +0100

    Add DOM manipulation

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const button = document.querySelector("#kupit");
+const emilka = document.querySelector("#img");
+const popis = document.querySelector("#popis");
 
 button.addEventListener("click", bobikTatik);
 
 function bobikTatik() {
   alert("Skrecka kupena!");
+  emilka.setAttribute("src", "./images/skreckaKupena.png");
+  popis.textContent = "Emilka: Claimnuta";
+  button.className = "disabled";
+  button.disabled = true;
 }
