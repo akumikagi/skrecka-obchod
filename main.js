c7ab39af20f72baa701b6fe563f5d89577164bba
akumi <akumikagi@gmail.com>
2023-12-24 00:57:26 +0100

    Add search function

--- main.js
+++ main.js
@@ -15,14 +15,30 @@ for(let item of nav) {
   menu.appendChild(li);
 }
 
-header.addEventListener("mouseenter", () => {
-  header.className = "active";
-  header.appendChild(menu);
-});
-header.addEventListener("mouseleave", () => {
-  header.className = "inactive";
-  header.removeChild(menu);
-})
+const search = document.createElement("input");
+search.setAttribute("type", "text");
+search.className = "search";
+search.value = "Search for animals!";
+
+const animals = [
+  { name: "Emilka", id: 1 },
+  { name: "Amalka", id: 2 },
+  { name: "Fliacik", id: 3 },
+  { name: "Sniezik", id: 4 },
+  { name: "Ela", id: 5 },
+  { name: "Yogi", id: 6 },
+  { name: "Zara", id: 7 },
+  { name: "Nufo", id: 8 },
+  { name: "Laba", id: 9 },
+  { name: "Fifo", id: 10 },
+  { name: "Slimy", id: 11 },
+];
+
+search.addEventListener("click", clearTip);
+search.addEventListener("keydown", findAnimal);
+
+header.addEventListener("mouseenter", mouseenter);
+header.addEventListener("mouseleave", mouseleave);
 
 footer.addEventListener("mouseenter", () => {
   footer.style.filter = "brightness(100%)";
@@ -30,3 +46,42 @@ footer.addEventListener("mouseenter", () => {
 footer.addEventListener("mouseleave", () => {
   footer.style.filter = "brightness(40%)";
 });
+
+function mouseenter() {
+  header.className = "active";
+  header.appendChild(search);
+  header.appendChild(menu);
+}
+
+function mouseleave() {
+  header.className = "inactive";
+  header.removeChild(search);
+  header.removeChild(menu);
+}
+
+function clearTip() {
+  if(search.value === "Search for animals!") {
+  search.value = "";
+  }
+  header.removeEventListener("mouseenter", mouseenter);
+  header.removeEventListener("mouseleave", mouseleave);
+}
+
+function findAnimal(e) {
+  if(search.value === "Press ESC to exit search mode!") {
+    search.value = "";
+  }
+  if(e.key === "Enter" && search.value !== "") {
+    let result = animals.find(item => item.name === search.value);
+    if(result === undefined) {
+      alert(`${search.value} not found!`);
+    } else {
+        alert(`${search.value} found! ${result.name}'s ID is ${result.id}`);
+    }
+    search.value = "Press ESC to exit search mode!";
+  }
+  else if(e.key === "Escape") {
+    header.addEventListener("mouseenter", mouseenter);
+    header.addEventListener("mouseleave", mouseleave);
+  }
+}
